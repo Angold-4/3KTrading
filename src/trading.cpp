3fb899651d748689886b07c6c8de5b4874a8af09
Angold-4 <wangold4w@gmail.com>
2022-01-20 20:09:31 +0800

    update parsing logic

--- src/trading.cpp
+++ src/trading.cpp
@@ -1,3 +1,78 @@
 #include "./trading.hpp"
 
 
+string Url::getUrl(string code) {
+    string interval = "1d"; 
+    string url = QUERY + code + '?' + "period1=" + to_string(this->getEpoch(this->start))
+	+ '&' + "period2=" + to_string(this->getEpoch(this->end)) + '&'
+	+ "interval=" + interval + "&events=history&includeAdjustedClose=true";
+    return url;
+}
+
+
+unordered_map<string, int> Parser::parse() {
+    unordered_map<string, int> ret = {};
+
+    string line;
+    bool bflag = true; // flag for buy in 12 1
+    bool sflag = true; // flag for sell in 3 4 5
+    istringstream iss(this->csvdata);
+    getline(iss, line); // do not need first line
+
+    // then iterate each line of this data
+    while (getline(iss, line)) {
+
+	// 1. deal with buy/sell indicator
+	if (line.substr(11, 4) == "null") continue; // null date
+
+	if (this->get_month(line) == 12 && bflag) {
+	    ret.insert(make_pair("12", 0));
+	    bflag = false;
+	    continue;
+	}
+
+	else if (this->get_month(line) == 2 && !bflag) {
+	    ret.insert(make_pair("2", 0));
+	    continue;
+	}
+
+	else if (this->get_month(line) == 3 && sflag) {
+	    ret.insert(make_pair("3", 0));
+	    continue;
+	}
+
+	else if (this->get_month(line) == 6 && !sflag) {
+	    ret.insert(make_pair("6", 0));
+	    continue;
+	}
+
+	// 2. insert them into ret umap
+	string date = this->get_date(line);
+	double avgvalue = this->ohlcavg(line);
+	// cout << "date: " << date << ", " " avg value: " << avgvalue << endl; // debug only
+
+	ret[date] = avgvalue;
+    }
+    return ret;
+}
+
+
+
+
+
+// Entry
+int main() {
+    string start = "01/10/1997";
+    string end   = "01/06/1998";
+    Url* UrlObj = new Url(start, end);
+    string url = UrlObj->getUrl("JPY=X");
+
+    CurlObj* CObj = new CurlObj(url);
+    string csvdata = CObj->getData();
+
+    Parser* ParserObj = new Parser(csvdata);
+    unordered_map<string, int> datevalue = ParserObj->parse();
+
+
+
+}
--- src/trading.hpp
+++ src/trading.hpp
@@ -37,8 +37,17 @@ private:
      *
      * translate DD/MM/YYYY into epoch (for yahoo finance)
      */
-    long getEpoch(string date);
+    long getEpoch(string date) {
+	// parse the DD/MM/YYYY into epoch
+	long ret;
+	struct tm mytm;
+	date += " 00:00:00";
+	strptime(date.c_str(), "%d/%m/%Y %H:%M:%S", &mytm);
 
+	ret = mktime(&mytm);
+
+	return ret + 28800;
+    }
 };
 
 
@@ -74,26 +83,73 @@ private:
 
 
 // Parser is the class that parse the csv data
+// parse csv data from Oct (prev year) to May (next year)
 class Parser {
 public:
-    Parser(string data, vector<int> buymonth, vector<int> sellmonth) : csvdata(data) {
+    Parser(string data) : csvdata(data) {}
 
-    }
-
+    /*
+     * Parser::parse
+     *
+     * for each day, fill the string and avg price (sum/4) 
+     * and then return them
+     */
+    unordered_map<string, int> parse();
+    
 
 private:
     string csvdata;
-    unordered_map<string, vector<int>> dateprices; // each date with its prices
+    int begm;
+    int endm;
+    int mbegin; // each month (divide them)
+    inline int get_month(string line) { return stoi(line.substr(5, 2)); }
+    inline string get_date(string line) { return line.substr(0, 10); }
 
     /*
      * Parser::ohlc
      *
-     * get ohlc price from dayprice line
+     * get ohlc avg price from dayprice line
      */
-    vector<int> ohlc(string dayprice);
+    double ohlcavg(string dayprice) {
+	int firstq = dayprice.find(',') + 1;
+	string tmp = "";
+	int count = 0;
+	double sum = 0;
+	for (int i = firstq; i < dayprice.size(); i++) {
+	    if (count == 4) {
+		// Termination Condition
+		break;
+	    }
+
+	    if (dayprice[i] == ',') {
+		sum += stof(tmp);
+		tmp = "";
+		count++;
+		continue;
+	    }
+	    tmp += dayprice[i];
+	}
+	return sum / 4;
+    }
 };
 
 // Trading is the class to simulate all kinds of trades
 class Trading {
+public:
+    Trading (unordered_map<string, double> datevalue) : yearval(datevalue) {}
+    
+    // #1 SMA trade simulation
+    void sma (int index);
+
+
+private:
+    unordered_map<string, double> yearval;
+
+    /*
+     * Trading::calcsma
+     *
+     * calculate sma with this interval
+     */
+    unordered_map<string, double> calcsma(int interval);
 
 };
